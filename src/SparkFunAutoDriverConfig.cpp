beb4b9ea723bb6ee08d8f36e7f07ac2f110c4d42
Mike Hord <mike.hord@sparkfun.com>
2016-08-16 15:21:01 -0600

    Added in a couple of fixes from the community based on pull requests

--- src/SparkFunAutoDriverConfig.cpp
+++ src/SparkFunAutoDriverConfig.cpp
@@ -215,15 +215,15 @@ void AutoDriver::setSwitchMode(int switchMode)
 {
   unsigned long configVal = getParam(CONFIG);
   // This bit is CONFIG 4, mask is 0x0010
-  configVal &= ~(0x0100);
+  configVal &= ~(0x0010);
   //Now, OR in the masked incoming value.
-  configVal |= (0x0100&switchMode);
+  configVal |= (0x0010 & switchMode);
   setParam(CONFIG, configVal);
 }
 
 int AutoDriver::getSwitchMode()
 {
-  return (int) (getParam(CONFIG) & 0x0100);
+  return (int) (getParam(CONFIG) & 0x0010);
 }
 
 // There are a number of clock options for this chip- it can be configured to
